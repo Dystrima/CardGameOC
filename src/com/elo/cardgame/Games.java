2939c0dd732bf788891a09cc4ac44cbe809ad660
Dystrima <138579225+Dystrima@users.noreply.github.com>
2024-01-18 10:23:08 +0100

    LowCardGameEvaluator

--- src/com/elo/cardgame/Games.java
+++ src/com/elo/cardgame/Games.java
@@ -3,12 +3,14 @@ package com.elo.cardgame;
 import com.elo.cardgame.controller.GameController;
 import com.elo.cardgame.games.GameEvaluator;
 import com.elo.cardgame.games.HighCardGameEvaluator;
+import com.elo.cardgame.games.LowCardGameEvaluator;
 import com.elo.cardgame.model.Deck;
 import com.elo.cardgame.view.View;
 
 public class Games {
     public static void main(String[] args) {
-        GameController gc = new GameController(new Deck(), new View(), new HighCardGameEvaluator()); // best card win
+//        GameController gc = new GameController(new Deck(), new View(), new HighCardGameEvaluator()); // highest card win
+        GameController gc = new GameController(new Deck(), new View(), new LowCardGameEvaluator()); // lowest card win
         gc.run();
     }
 }
